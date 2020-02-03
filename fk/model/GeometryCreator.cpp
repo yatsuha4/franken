f33ec30d3536661c24dbebe85badc3a1405cf032
YASUI Kentarow <yasuikentarow@gmail.com>
2020-02-04 08:22:27 +0900

    GeometryCreatorクラス追加

---
+++ fk/model/GeometryCreator.cpp
@@ -0,0 +1,46 @@
+﻿/***********************************************************************//**
+	@file
+***************************************************************************/
+#include "fk/model/GeometryCreator.hpp"
+
+namespace fk {
+namespace model {
+/***********************************************************************//**
+	@brief 
+***************************************************************************/
+size_t GeometryCreator::appendVertex(const Vertex& vertex) {
+  auto index = vertexes_.size();
+  vertexes_.emplace_back(vertex);
+  return index;
+}
+/***********************************************************************//**
+	@brief 
+***************************************************************************/
+void GeometryCreator::appendLine(const Vertex& p1, const Vertex& p2) {
+  indexes_.push_back(appendVertex(p1));
+  indexes_.push_back(appendVertex(p2));
+}
+/***********************************************************************//**
+	@brief 
+***************************************************************************/
+GeometryPtr GeometryCreator::create(Renderer& renderer, 
+                                    Geometry::Mode mode) {
+  auto geometry = std::make_shared<Geometry>();
+  setup(*geometry, renderer, mode);
+  return geometry;
+}
+/***********************************************************************//**
+	@brief 
+***************************************************************************/
+void GeometryCreator::setup(Geometry& geometry, Renderer& renderer, 
+                            Geometry::Mode mode) {
+  geometry.setup(renderer, mode, vertexes_.size(), vertexes_.data(), 
+                 indexes_.size(), indexes_.data());
+  vertexes_.clear();
+  indexes_.clear();
+}
+/***********************************************************************//**
+	$Id$
+***************************************************************************/
+}
+}
---
+++ fk/model/GeometryCreator.hpp
@@ -0,0 +1,36 @@
+﻿/***********************************************************************//**
+	@file
+***************************************************************************/
+#pragma once
+
+#include "fk/model/Geometry.hpp"
+#include "fk/model/Vertex.hpp"
+
+namespace fk {
+namespace model {
+/***********************************************************************//**
+	@brief ジオメトリ生成クラス
+***************************************************************************/
+class GeometryCreator {
+ private:
+  std::vector<Vertex> vertexes_;
+  std::vector<Geometry::index_t> indexes_;
+
+ public:
+  GeometryCreator() = default;
+  ~GeometryCreator() = default;
+
+  FK_ACCESSOR(Vertexes, vertexes_);
+  FK_ACCESSOR(Indexes, indexes_);
+
+  size_t appendVertex(const Vertex& vertex);
+  void appendLine(const Vertex& p1, const Vertex& p2);
+
+  GeometryPtr create(Renderer& renderer, Geometry::Mode mode);
+  void setup(Geometry& geometry, Renderer& renderer, Geometry::Mode mode);
+};
+/***********************************************************************//**
+	$Id$
+***************************************************************************/
+}
+}
