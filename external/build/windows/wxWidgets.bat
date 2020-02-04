48e81b844346d5ea5efaae3cbba627615e886761
YASUI Kentarow <yasuikentarow@gmail.com>
2020-02-04 19:33:18 +0900

    windows用のwxWidgetsビルドバッチを追加

---
+++ external/build/windows/wxWidgets.bat
@@ -0,0 +1,9 @@
+﻿set CURDIR=%~dp0
+set SRCDIR=%CURDIR%..\..\src\wxWidgets
+
+pushd "%SRCDIR%\build\msw"
+nmake /a /f makefile.vc clean
+nmake /a /f makefile.vc BUILD=release clean
+nmake /a /f makefile.vc 
+nmake /a /f makefile.vc BUILD=release
+popd
